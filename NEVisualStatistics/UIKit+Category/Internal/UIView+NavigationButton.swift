23ee847315daf9c90f08a2b18d277c2733f45428
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2018-04-13 09:57:03 +0800

    finish navigationButton category

--- NEVisualStatistics/UIKit+Category/Internal/UIView+NavigationButton.swift
+++ NEVisualStatistics/UIKit+Category/Internal/UIView+NavigationButton.swift
@@ -9,9 +9,11 @@
 extension UIView {
     var navigationButtonEventID: String {
         get {
+            guard let actions: [Any] = value(forKey: "targetActions") as? [Any] else { return "" }
+            guard let action = actions.first as? NSObject else { return "" }
+            guard let target = action.value(forKey: "target") as? UIBarButtonItem else { return "" }
             
-            
-            return "1"
+            return target.accessibilityIdentifier ?? target.accessibilityLabel ?? ""
         }
     }
 }
